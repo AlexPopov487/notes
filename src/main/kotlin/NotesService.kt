721e3ac285bac5cc5859e9d32f344c7741d70463
Alex Popov <apopov974@gmail.com>
2020-12-31 21:22:24 +0500

    tests implemented

--- src/main/kotlin/NotesService.kt
+++ src/main/kotlin/NotesService.kt
@@ -1,37 +1,43 @@
 class NotesService {
-    val notesList = mutableListOf<Note>()
-    private val commentsList = mutableListOf<Comment>()
-    private val commentsDustbin = mutableListOf<Comment>()
+    private val _notesList = mutableListOf<Note>()
+    val notesList: List<Note>
+        get() = _notesList
+    private val _commentsList = mutableListOf<Comment>()
+    val commentsList: List<Comment>
+        get() = _commentsList
+    private val _commentsDustbin = mutableListOf<Comment>()
+    val commentsDustbin: List<Comment>
+        get() = _commentsDustbin
     private val notesDustbin = mutableListOf<Note>()
 
 
     fun showAllNotes() {
-        if (notesList.isEmpty()) println("You have no notes!") else println(notesList)
+        if (_notesList.isEmpty()) println("You have no notes!") else println(_notesList)
     }
 
     fun findNoteById(id: Long): Note {
-        for (item in notesList) {
+        for (item in _notesList) {
             if (item.id == id) return item
         }
         throw IllegalArgumentException("Note with the provided id doesn't exist!")
     }
 
     fun addNote(note: Note) {
-        val noteId: Long = if (notesList.isNotEmpty()) notesList.last().id + 1 else 1
+        val noteId: Long = if (_notesList.isNotEmpty()) _notesList.last().id + 1 else 1
         note.id = noteId
-        notesList.add(note)
+        _notesList.add(note)
     }
 
     fun deleteNote(note: Note) {
-        if (notesList.contains(note)) {
+        if (_notesList.contains(note)) {
 
-            val iterator = commentsList.iterator()
+            val iterator = _commentsList.iterator()
             while (iterator.hasNext()) {
                 val oldValue = iterator.next()
                 if (oldValue.noteId == note.id) iterator.remove()
             }
 
-            notesList.remove(note)
+            _notesList.remove(note)
             println("Note \"${note.title}\" and all its comments were successfully deleted!")
         } else {
             println("You cannot delete note that doesn't exist in a notes list!")
@@ -39,11 +45,11 @@ class NotesService {
     }
 
     fun editNote(editedNote: Note, oldNoteId: Long) {
-        for (item in notesList) {
+        for (item in _notesList) {
             if (item.id == oldNoteId) {
                 editedNote.id = item.id
                 editedNote.isEdited = true
-                notesList[notesList.indexOf(item)] = editedNote
+                _notesList[_notesList.indexOf(item)] = editedNote
                 println("Note was successfully edited")
                 return
             }
@@ -52,13 +58,13 @@ class NotesService {
     }
 
     fun showAllComments() {
-        if (commentsList.isEmpty()) println("You have no comments at all!") else println(commentsList)
+        if (_commentsList.isEmpty()) println("You have no comments at all!") else println(_commentsList)
     }
 
     fun showCommentsByNote(noteId: Long) {
         var isNoteFound = false
 
-        for (item in notesList) {
+        for (item in _notesList) {
             if (item.id == noteId) {
                 isNoteFound = true
                 break
@@ -68,7 +74,7 @@ class NotesService {
         if (isNoteFound) {
             val commentsFoundByNote = mutableListOf<Comment>()
 
-            for (comment in commentsList) {
+            for (comment in _commentsList) {
                 if (comment.noteId == noteId) commentsFoundByNote.add(comment)
             }
             println(commentsFoundByNote)
@@ -81,7 +87,7 @@ class NotesService {
         var isCommentValid = false
         var idForComment: Long = 0
 
-        for (item in notesList) {
+        for (item in _notesList) {
             if (item.id == comment.noteId) {
                 isCommentValid = true
                 idForComment = item.id
@@ -90,26 +96,26 @@ class NotesService {
         }
         if (isCommentValid) {
             comment.noteId = idForComment
-            commentsList.add(comment)
-            comment.commentIndex = commentsList.lastIndex
+            _commentsList.add(comment)
+            comment.commentIndex = _commentsList.lastIndex
         } else print("You cannot create a comment for a non-existing note!")
     }
 
     fun deleteComment(comment: Comment) {
-        if (commentsList.contains(comment)) {
-            commentsDustbin.add(comment)
-            comment.commentIndex = commentsList.indexOf(comment)
-            commentsList.remove(comment)
+        if (_commentsList.contains(comment)) {
+            _commentsDustbin.add(comment)
+            comment.commentIndex = _commentsList.indexOf(comment)
+            _commentsList.remove(comment)
             println("Comment was successfully moved to the dustbin! You can restore it if you want")
         } else {
-            println("Failed to find a note for deletion")
+            println("Failed to find a comment for deletion")
         }
     }
 
     fun restoreComment(comment: Comment) {
-        if (commentsDustbin.contains(comment)) {
-            commentsList.add(comment.commentIndex, comment)
-            commentsDustbin.remove(comment)
+        if (_commentsDustbin.contains(comment)) {
+            _commentsList.add(comment.commentIndex, comment)
+            _commentsDustbin.remove(comment)
             println("The removed comment was restored and index ${comment.commentIndex + 1}, as if it was never deleted")
         } else {
             println("Failed to find a comment to restore")
@@ -117,11 +123,11 @@ class NotesService {
     }
 
     fun editComment(editedComment: Comment, commentToReplace: Comment) {
-        for (item in commentsList) {
+        for (item in _commentsList) {
             if (item == commentToReplace && item.noteId == editedComment.noteId) {
                 editedComment.commentIndex = item.commentIndex
                 editedComment.isEdited = true
-                commentsList[commentsList.indexOf(item)] = editedComment
+                _commentsList[_commentsList.indexOf(item)] = editedComment
                 println("Comment was successfully edited")
                 return
             }
---
+++ src/test/kotlin/NotesServiceTest.kt
@@ -0,0 +1,232 @@
+import org.junit.Assert.*
+import org.junit.Test
+import java.lang.IllegalArgumentException
+
+class NotesServiceTest {
+    val notesService = NotesService()
+
+    @Test
+    fun findNoteById_success(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+
+        notesService.addNote(firstNote)
+
+        val result = notesService.findNoteById(1)
+
+        assertEquals(firstNote, result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun findNoteById_throwException(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+
+        notesService.addNote(firstNote)
+
+        notesService.findNoteById(2)
+    }
+
+    @Test
+    fun addNote_ToEmptyList(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val expected:Boolean = firstNote.id == 1L
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun addNote_ToFilledList(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val secondNote = Note(title = "Second note", text = "Hey!")
+        notesService.addNote(secondNote)
+        val expected:Boolean = secondNote.id == 2L
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun deleteNote_success(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+        val secondNote = Note(title = "Second note", text = "Hey!")
+        notesService.addNote(secondNote)
+
+        notesService.deleteNote(secondNote)
+
+        val expected = notesService.notesList.contains(secondNote)
+
+        assertFalse(expected)
+    }
+
+    @Test
+    fun deleteNote_noNoteToDelete(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+        val secondNote = Note(title = "Second note", text = "Hey!")
+
+        notesService.deleteNote(secondNote)
+        val expected = notesService.notesList.contains(secondNote)
+
+        assertFalse(expected)
+    }
+
+    @Test
+    fun editNote_success(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+        val secondNote = Note(title = "Second note", text = "Hey!")
+        notesService.addNote(secondNote)
+        val thirdNote = Note(title = "Note to replace the old one", text = "Luls")
+
+        notesService.editNote(thirdNote, 2)
+
+        val expected = notesService.notesList[1].isEdited && notesService.notesList.contains(thirdNote)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun editNote_noNoteToEdit(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+        val secondNote = Note(title = "Second note", text = "Hey!")
+//        notesService.addNote(secondNote)
+        val thirdNote = Note(title = "Note to replace the old one", text = "Luls")
+
+        notesService.editNote(thirdNote, 2)
+
+        val expected = notesService.notesList.contains(thirdNote)
+
+        assertFalse(expected)
+    }
+
+    @Test
+    fun createComment_success(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+
+        notesService.createComment(commentForFirstNote)
+
+        val expected = notesService.commentsList.contains(commentForFirstNote)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun createComment_commentIdDoesntMatchNoteId(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 2, message = "That's a great note there!")
+
+        notesService.createComment(commentForFirstNote)
+
+        val expected = notesService.commentsList.contains(commentForFirstNote)
+
+        assertFalse(expected)
+    }
+
+    @Test
+    fun deleteComment_successful(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+        notesService.createComment(commentForFirstNote)
+        notesService.deleteComment(commentForFirstNote)
+
+        val expected = !notesService.commentsList.contains(commentForFirstNote)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun deleteComment_noCommentToDelete(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+//        notesService.createComment(commentForFirstNote)
+        notesService.deleteComment(commentForFirstNote)
+
+        val expected = !notesService.commentsDustbin.contains(commentForFirstNote)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun restoreComment_successful(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+        notesService.createComment(commentForFirstNote)
+        notesService.deleteComment(commentForFirstNote)
+
+        val expectedBeforeRestored = notesService.commentsDustbin.contains(commentForFirstNote)
+
+        notesService.restoreComment(commentForFirstNote)
+
+        val expectedAfterRestored = !notesService.commentsDustbin.contains(commentForFirstNote) &&
+                notesService.commentsList.contains(commentForFirstNote)
+
+
+        assertTrue(expectedBeforeRestored)
+        assertTrue(expectedAfterRestored)
+    }
+
+    @Test
+    fun restoreComment_noCommentInDustbin(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+        notesService.createComment(commentForFirstNote)
+//        notesService.deleteComment(commentForFirstNote)
+
+        val expected = !notesService.commentsDustbin.contains(commentForFirstNote)
+
+        notesService.restoreComment(commentForFirstNote)
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun editNote_successful(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+        notesService.createComment(commentForFirstNote)
+
+        val newComment = Comment(1, "I'm here to replace the first comment")
+
+        notesService.editComment(newComment, commentForFirstNote)
+
+        val expected = notesService.commentsList[0].isEdited
+
+        assertTrue(expected)
+    }
+
+    @Test
+    fun editNote_commentIdsDontMatch(){
+        val firstNote = Note(title = "First note", text = "Hey!")
+        notesService.addNote(firstNote)
+
+        val commentForFirstNote = Comment(noteId = 1, message = "That's a great note there!")
+        notesService.createComment(commentForFirstNote)
+
+        val newComment = Comment(2, "I'm here to replace the first comment")
+
+        notesService.editComment(newComment, commentForFirstNote)
+
+        val expected = !notesService.commentsList[0].isEdited
+
+        assertTrue(expected)
+    }
+}
